aa3f887a7db8266a481d3a19fdd4a617ff11a3d7
NamNguyen310801 <92281093+NamNguyen310801@users.noreply.github.com>
2022-11-10 21:24:18 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+var btnSuccess = document.querySelector('.control .seccess');
+var btnWarning = document.querySelector('.control .warning');
+var btnErr = document.querySelector('.control .error');
+var message = document.querySelector('.toast .message');
